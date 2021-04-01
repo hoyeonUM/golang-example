e7d4aad1443975f854f0fcb99e5c474a6cc71522
hyum02 <hyum02@simplexi.com>
2021-04-02 00:07:13 +0900

    1. test name fix

--- example_limit_gorutine/concurrent_test.go
+++ example_limit_gorutine/concurrent_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestCrawler(t *testing.T) {
+func TestConcurrent(t *testing.T) {
 	t.Run("given concurrent count 10 and total count 100 when increase number 10 and wait time is 300ms then over execute time ge 1500ms", func(t *testing.T) {
 		startTime := time.Now()
 		example_limit_gorutine.Run(2, 100)
