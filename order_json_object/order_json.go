d331eada274c57b9201b0f0549bfc13b32e89e16
hyum02 <hyum02@simplexi.com>
2021-04-02 20:30:26 +0900

    add example

---
+++ order_json_object/order_json.go
@@ -0,0 +1,36 @@
+package order_json_object
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+type OrderJsonMap struct {
+	Order []string
+	Map   map[string]string
+}
+
+func (om OrderJsonMap) MarshalJSON() ([]byte, error) {
+	var b []byte
+	buf := bytes.NewBuffer(b)
+	buf.WriteRune('{')
+	l := len(om.Order)
+	for i, key := range om.Order {
+		km, err := json.Marshal(key)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(km)
+		buf.WriteRune(':')
+		vm, err := json.Marshal(om.Map[key])
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(vm)
+		if i != l-1 {
+			buf.WriteRune(',')
+		}
+	}
+	buf.WriteRune('}')
+	return buf.Bytes(), nil
+}
---
+++ order_json_object/order_json_test.go
@@ -0,0 +1,30 @@
+package order_json_object_test
+
+import (
+	"encoding/json"
+	"github.com/hoyeonUM/golang-example/order_json_object"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConcurrent(t *testing.T) {
+	t.Run(`given string map first key is b and second key is a when parse json then first key is a and second key is b`, func(t *testing.T) {
+		notOrderJson := make(map[string]string)
+		notOrderJson["b"] = "bbb"
+		notOrderJson["a"] = "aaa"
+		b, _ := json.Marshal(notOrderJson)
+		assert.Equal(t, `{"a":"aaa","b":"bbb"}`, string(b))
+	})
+
+	t.Run(`given string map first key is b and second key is a when parse order json then first key is b and second key is a`, func(t *testing.T) {
+		notOrderJson := make(map[string]string)
+		notOrderJson["b"] = "bbb"
+		notOrderJson["a"] = "aaa"
+
+		OrderJson := order_json_object.OrderJsonMap{}
+		OrderJson.Order = []string{"b", "a"}
+		OrderJson.Map = notOrderJson
+		b, _ := json.Marshal(OrderJson)
+		assert.Equal(t, `{"b":"bbb","a":"aaa"}`, string(b))
+	})
+}
